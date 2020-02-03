1b6fe1dee752a2ecef73bb00f27d5f24af38c959
bibek7777 <bibekstha084@gmail.com>
2020-02-03 19:59:56 +0545

    first commit

---
+++ NepaliDateHelper.php
@@ -0,0 +1,182 @@
+<?php
+namespace App\Utils;
+
+use Exception;
+use App\Utils\DateHelper;
+
+class NepaliDateHelper  
+{
+
+    private $date;
+    private $dateHelper;
+
+    public function __construct($date) {
+        $this->date = $date;
+        $this->dateHelper = new DateHelper();
+    }
+
+    //only add days
+    public function addDays($days, $date=null)
+    {
+        $dateData = explode('/', $date??$this->date);
+        $given_day = $dateData[2];
+        $year = $dateData[0];
+        $month = $dateData[1];
+
+        $total_days_in_month = $this->dateHelper->getDateTable($year, $month)->days_bs;
+        
+        if ($given_day > $total_days_in_month) {
+            throw new Exception("Days exceeds total days given month", 1);
+        }
+
+        // $days_left_in_month = $total_days_in_month - $given_day;
+    
+
+        $addedDays = $given_day + $days;
+        
+        if ($addedDays > $total_days_in_month) {
+            $extra_days = $addedDays - $total_days_in_month;
+            $month++;
+            if ($month > 12) {
+                $month = $month - 12;
+                $year++;
+            }
+            $newDate = $year .'/'. $month .'/'. '01';
+            $newDayToadd = $extra_days - 1;
+            return $this->addDays($newDayToadd, $newDate);
+        }
+        return $year .'/'. $month .'/'. $addedDays; 
+    }
+
+    //only add months
+    public function addMonths($month_data, $date = null)
+    {
+        $dateData = explode('/', $date??$this->date);
+        $given_day = $dateData[2];
+        $year = $dateData[0];
+        $month = $dateData[1];
+
+        $newMonth = $month + $month_data;
+
+        $newDay = $given_day - 1;
+
+        if ($newMonth > 12) {
+            $newMonth = $newMonth - 12;
+            $year++;
+            $newDate = $year .'/'. '01' .'/'. $given_day;
+            return $this->addMonths($newMonth - 1, $newDate);
+        }
+        
+        $total_days_in_month = $this->dateHelper->getDateTable($year, $newMonth)->days_bs;
+    
+
+        if ($newDay > $total_days_in_month) {
+            $newDay = $total_days_in_month;
+        }
+
+        return $year .'/'. $newMonth .'/'. $newDay;
+
+    }
+
+    //only add years
+    public function addYears($year_data, $date = null)
+    {
+        $dateData = explode('/', $date??$this->date);
+        $given_day = $dateData[2];
+        $year = $dateData[0];
+        $month = $dateData[1];
+
+        $newYear = $year + $year_data;
+        $newMonth = $month - 1;
+        $newDay = $given_day - 1;
+
+        return $this->addYearHelper($newYear, $newMonth, $newDay);
+    }
+
+    //helper function for adding year
+    public function addYearHelper($newYear, $newMonth, $newDay)
+    {
+        //decrease year 
+        if ($newMonth < 1) {
+            $newYear--;
+            $newMonth = 12;
+
+            $total_days_in_month = $this->dateHelper->getDateTable($newYear, $newMonth)->days_bs;
+            if($newDay > $total_days_in_month){
+                $newDay = $total_days_in_month;
+            } elseif ($newDay < 1) {//decrease month 
+                $new_date = $this->checkMonth($newYear, $newMonth, $newDay);
+                $newYear = $new_date[0];
+                $newMonth = $new_date[1];
+                $newDay = $new_date[2];
+            }
+        }
+
+        //decrease month
+        if($newDay < 1)
+        {
+            $new_date = $this->checkMonth($newYear, $newMonth, $newDay);
+            $newYear = $new_date[0];
+            $newMonth = $new_date[1];
+            $newDay = $new_date[2];
+        }
+
+        return ( $newYear .'/'. $newMonth .'/'. $newDay);
+    }
+
+    //decrease month by 1 if day is less than 1
+    public function checkMonth($newYear, $newMonth, $newDay)
+    {
+        $newMonth--;
+        if($newMonth < 1){
+            $newYear--;
+            $newMonth = 12;
+            $total_days_in_month = $this->dateHelper->getDateTable($newYear, $newMonth)->days_bs;
+            $newDay = $total_days_in_month;
+        } else {
+            $newDay = $this->dateHelper->getDateTable($newYear, $newMonth)->days_bs;
+        }
+        return [$newYear, $newMonth, $newDay];
+    }
+
+    //add all
+    public function addAll($years, $months, $days)
+    {
+        $dateAfterDays = $this->addDays($days); 
+        $dateAfterMonths = $this->addMonths($months, $dateAfterDays);
+        $dateAfterYears = $this->addYears($years, $dateAfterMonths);
+        return $this->addDays('1', $dateAfterYears);
+    }
+
+    //add months and years
+    public function addYearsAndMonths($years, $months)
+    {
+        $dateAfterMonths = $this->addMonths($months);
+        $dateAfterYears = $this->addYears($years, $dateAfterMonths);
+        return $this->addDays('1', $dateAfterYears);
+    }
+
+    //add days and years
+    public function addYearsAndDays($years, $days)
+    {
+        $dateAfterDays = $this->addDays($days);
+        $dateAfterYears = $this->addYears($years, $dateAfterDays);
+        return $dateAfterYears;
+    }
+
+    //add days and months
+    public function addMonthsAndDays($months, $days)
+    {
+        $dateAfterDays = $this->addDays($days);
+        $dateAfterMonths = $this->addMonths($months, $dateAfterDays);
+        return $dateAfterMonths;
+    }
+
+    // //get data from table
+    // public function getDateTable($year, $month)
+    // {
+    //     $dateTable = DateTable::where('year_bs', $year)->where('month_bs', $month)->first();
+    //     return $dateTable;
+    // }
+
+}
---
+++ SubtractNepaliDate.php
@@ -0,0 +1,202 @@
+<?php 
+
+namespace App\Utils;
+
+use App\Utils\DateHelper;
+use App\Utils\NepaliDateHelper;
+
+
+class SubtractNepaliDate
+{
+    private $date;
+    private $dateHelper;
+
+    public function __construct($date) {
+        $this->date = $date;
+        $this->dateHelper = new DateHelper();
+    }
+
+    //subtract days
+    public function subDays($days, $date=null)
+    {
+        $dateDetail = $this->dateHelper->getDayMonthYear($date??$this->date);
+        $day = $dateDetail['day'];
+        $month = $dateDetail['month'];
+        $year = $dateDetail['year'];
+
+        $subtracted_days = $day - $days;
+
+        $noOfDaysInMonth = $this->dateHelper->getDateTable($year, $month)->days_bs;
+
+        if($subtracted_days < 1)
+        {
+            $new_days = $noOfDaysInMonth + $subtracted_days;
+            $month--;
+            if($month < 1)
+            {
+                $month = 12;
+                $year--;
+            }
+            $new_date = $year .'/'. $month .'/'. $noOfDaysInMonth;
+            $new_day = $noOfDaysInMonth - $new_days;
+            return $this->subDays($new_day, $new_date);
+        }
+
+        return ($year .'/'. $month .'/'. $subtracted_days);
+        
+    }
+
+    //subtract months
+    public function subMonths($months, $date=null)
+    {
+        $dateDetail = $this->dateHelper->getDayMonthYear($date??$this->date);
+        $day = $dateDetail['day'];
+        $month = $dateDetail['month'];
+        $year = $dateDetail['year'];
+
+        $subtracted_months = $month - $months;
+
+        if($subtracted_months < 1)
+        {
+            return $this->monthRecursion($year, $day, $subtracted_months);
+        }
+
+        //if day is less than or equal 1, decrease month and also decrease year if month is less than 1
+        if($day <= 1)
+        {
+            $month--;
+            $noOfDaysInMonth = $this->dateHelper->getDateTable($year, $month)->days_bs;
+            $day = $noOfDaysInMonth;
+            if($month < 1)
+            {
+                return $this->monthRecursion($year, $day, $month);               
+            }
+        }
+
+        //if month is greater than 12, add the negative month
+        if($subtracted_months > 12)
+        {
+            $subtracted_months = $month + $months;
+            if($subtracted_months < 1)
+            {
+                return $this->monthRecursion($year, $day, $subtracted_months);
+            }
+        }
+
+        //must be here, other wise day will be deducted because of recursive function
+        $day = $day - 1;
+
+        return ($year .'/'. $subtracted_months .'/'. $day);
+    }
+
+    //calls the subMonths recursively
+    public function monthRecursion($year, $day, $subtracted_months)
+    {
+        $new_date = $this->monthHelper($year, $day);
+        return $this->subMonths($subtracted_months, $new_date);
+    }
+
+    //deducting year by 1 if month is less than 1
+    public function monthHelper($year, $day)
+    {
+        $month = 12;
+        $year--;
+        $new_date = $year .'/'. $month .'/'. $day;
+        return $new_date;
+    }
+
+    //subtracting year
+    public function subYears($years, $date = null)
+    {
+        $dateDetail = $this->dateHelper->getDayMonthYear($date??$this->date);
+        $day = $dateDetail['day'];
+        $month = $dateDetail['month'];
+        $year = $dateDetail['year'];
+
+        $year = $year - $years;
+        $month = $month - 1;
+        $day = $day - 1;
+
+        //decrease year by 1, if month is less than 1
+        if($month < 1)
+        {
+            $year--;
+            $month = 12;
+            $noOfDaysInMonth = $this->dateHelper->getDateTable($year, $month)->days_bs;
+            if($day > $noOfDaysInMonth)
+            {
+                $day = $noOfDaysInMonth;
+            } elseif ($day < 1) {
+                $new_date = $this->checkMonth($year, $month, $day);
+                $year = $new_date[0];
+                $month = $new_date[1];
+                $day = $new_date[2];
+            }
+        }
+
+        //decrease month by 1, if day is less than 1
+        if($day < 1)
+        {
+            $new_date = $this->checkMonth($year, $month, $day);
+            $year = $new_date[0];
+            $newMonth = $new_date[1];
+            $day = $new_date[2];
+        }
+
+        return ($year .'/'. $month .'/'. $day);
+         
+    }
+
+    //decrease month by 1 if day is less than 1
+    public function checkMonth($newYear, $newMonth, $newDay)
+    {
+        $newMonth--;
+        if($newMonth < 1){
+            $newYear--;
+            $newMonth = 12;
+            $noOfDaysInMonth = $this->dateHelper->getDateTable($newYear, $newMonth)->days_bs;
+            $newDay = $noOfDaysInMonth;
+        } else {
+            $newDay = $this->dateHelper->getDateTable($newYear, $newMonth)->days_bs;
+        }
+        return [$newYear, $newMonth, $newDay];
+    }
+
+    //sub all
+    public function subAll($years, $months, $days)
+    {
+        $dateAfterDays = $this->subDays($days); 
+        $dateAfterMonths = $this->subMonths($months, $dateAfterDays);
+        $dateAfterYears = $this->subYears($years, $dateAfterMonths);
+        $nepaliDate = new NepaliDateHelper($dateAfterYears);
+        return $nepaliDate->addDays(1);
+    }
+
+    //sub months and years
+    public function subYearsAndMonths($years, $months)
+    {
+        $dateAfterMonths = $this->subMonths($months);
+        $dateAfterYears = $this->subYears($years, $dateAfterMonths);
+        $nepaliDate = new NepaliDateHelper($dateAfterYears);
+        return $nepaliDate->addDays(1);
+    }
+
+    //sub days and years
+    public function subYearsAndDays($years, $days)
+    {
+        $dateAfterDays = $this->subDays($days);
+        $dateAfterYears = $this->subYears($years, $dateAfterDays);
+        return $dateAfterYears;
+    }
+
+    //sub days and months
+    public function subMonthsAndDays($months, $days)
+    {
+        $dateAfterDays = $this->subDays($days);
+        $dateAfterMonths = $this->subMonths($months, $dateAfterDays);
+        return $dateAfterMonths;
+    }
+
+}
+
+
